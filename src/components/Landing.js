7667a42240aea5ad4d57e7beaaed6282de72334f
Rom Vic <technoyguy@gmail.com>
2018-06-09 16:24:36 -0700

    sections added

--- src/components/Landing.js
+++ src/components/Landing.js
@@ -1,7 +1,14 @@
 import React from 'react';
 import Background from '../images/icon_join_hands.jpg';
 import People from '../images/icon_people_circle.png';
+
+import Feature1 from '../images/feature_hourglass.png';
+import Feature2 from '../images/feature_exchange.png';
+import Feature3 from '../images/feature_rewards.png';
+import Feature4 from '../images/feature_purchase.png';
+import TechLogo from '../images/icon_deep_technology.png'
 import * as routes from '../constants/routes'
+import './Landing.css';
 
 let sectionStyleHeaderMasthead = {
   width: "100%",
@@ -43,6 +50,103 @@ const Landing = () =>
         </header>
        
     </div>
+    <div className="features" style={{paddingTop:'380px'}}>
+    <section className="content-section bg-primary text-white text-center" id="services">
+      <div className="container">
+        <div className="content-section-heading">
+          
+          <h2 className="mb-5">What Content Ether Features</h2>
+        </div>
+        <div className="row">
+          <div className="col-lg-3 col-md-6 mb-5 mb-lg-0">
+            <span className="service-icon rounded-circle mx-auto mb-3">
+            <img src={Feature1} alt='feature1' />
+            </span>
+            <h4>
+              <strong>Analytics</strong>
+            </h4>
+            <p className="text-faded mb-0">Measure & track media consumption via Tokens</p>
+          </div>
+          <div className="col-lg-3 col-md-6 mb-5 mb-lg-0">
+            <span className="service-icon rounded-circle mx-auto mb-3">
+            <img src={Feature2} alt='feature2' />
+            </span>
+            <h4>
+              <strong>Syndication</strong>
+            </h4>
+            <p className="text-faded mb-0">Smart contracts for Media syndication and exchange</p>
+          </div>
+          <div className="col-lg-3 col-md-6 mb-5 mb-md-0">
+            <span className="service-icon rounded-circle mx-auto mb-3">
+            <img src={Feature3} alt='feature3' />
+            </span>
+            <h4>
+              <strong>Rewards</strong>
+            </h4>
+            <p className="text-faded mb-0">Reward for sharing via Tokens</p>
+          </div>
+          <div className="col-lg-3 col-md-6">
+            <span className="service-icon rounded-circle mx-auto mb-3">
+            <img src={Feature4} alt='feature' />
+            </span>
+            <h4>
+              <strong>Buy Token</strong>
+            </h4>
+            <p className="text-faded mb-0">Purchase Media via Tokens</p>
+          </div>
+        </div>
+      </div>
+    </section>
+    </div>
+
+    <section className="callout">
+      <div className="container text-center">
+          <div className="row">
+          <div className="col-lg-6 col-md-6 mb-5 mb-lg-0" >
+           <div className="image">
+            <img src={TechLogo} alt='TechLogo' />
+            </div>
+          </div>
+          <div className="col-lg-6 col-md-6 mb-5 mb-lg-0">
+          <h2 className="mx-auto mb-5">Deep Technology</h2>
+          <p class="lead mb-5">The platform was designed by seasoned entrepreneurial and technical team of Industry veterans.
+            <div className="row">
+               <div className="col-lg-2 " >  
+                  <img width="100%" src={TechLogo} alt='TechLogo' />
+               </div>
+                <div className="col-lg-8 " >
+                  Built on top of Etherum Blockchain
+               </div>
+            </div>  
+            <div className="row">
+               <div className="col-lg-2 " >  
+                  <img width="100%" src={TechLogo} alt='TechLogo' />
+               </div>
+                <div className="col-lg-8 " >
+                  Deep learning of audience clusters
+               </div>
+            </div> 
+            <div className="row">
+               <div className="col-lg-2 " >  
+                  <img width="100%" src={TechLogo} alt='TechLogo' />
+               </div>
+                <div className="col-lg-8 " >
+                  Powerful Media monetization
+               </div>
+            </div> 
+            <div className="row">
+               <div className="col-lg-2 " >  
+                  <img width="100%" src={TechLogo} alt='TechLogo' />
+               </div>
+                <div className="col-lg-8 " >
+                  Tracking viral propagation of media
+               </div>
+            </div>  
+          </p>
+           </div>
+        </div>
+      </div>
+    </section>
   </div>
 
  
